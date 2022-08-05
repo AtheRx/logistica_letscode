c1f20b24e576ce8b529294ed0b758050a8ecbe80
maxmfernan <maxmfernan@gmail.com>
2022-08-05 16:11:34 -0300

    Pequena modificação para contornar o problema das threads no intelliJ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import models.Relatorio;
 
 public class Main {
     public static void main(String[] args) {
+
         boolean keepRunning = true;
         Relatorio relatorio = new Relatorio(Configuracoes.nomeRelatorio());
         Configuracoes.carregar();
@@ -28,19 +29,65 @@ public class Main {
         }
 
         lagar.run();
-        while(keepRunning){
 
+//        Thread threadControla = new Thread(new ControlaFechamentoRelatorio(relatorio));
+//        threadControla.setDaemon(true);
+//        try{
+//            threadControla.start();
+//        }finally{
+//            //System.out.println("Teste");
+//        }
+
+        while(keepRunning) {
+
+            //System.out.println(Thread.currentThread());
             try {
-                if (Thread.activeCount() <= 1) {
+                //System.out.println(Thread.activeCount());
+
+                if (Thread.activeCount() <= 2) {
+                    Thread.sleep(2_000);
                     relatorio.fecharRegistro();
                     keepRunning = false;
 
+
                 }
             } catch (Exception e) {
-                
+
                 System.out.printf("O Arquivo já foi fechado.%n" + e.getStackTrace());
-            } 
+            }
         }
 
+
     }
+
+//    static class ControlaFechamentoRelatorio implements Runnable{
+//        Relatorio relatorio;
+//        public ControlaFechamentoRelatorio(Relatorio relatorio){
+//            this.relatorio = relatorio;
+//        }
+//        @Override
+//        public void run() {
+//            boolean keepRunning = true;
+//            while(keepRunning){
+//
+//                System.out.println(Thread.currentThread());
+//                try {
+//                    //System.out.println(Thread.activeCount());
+//
+//                    if (Thread.activehoCount() <= 2) {
+//                        Thread.sleep(2_000);
+//                        relatorio.fecharRegistro();
+//                        //keepRunning = false;
+//
+//
+//
+//                    }
+//                } catch (Exception e) {
+//
+//                    System.out.printf("O Arquivo já foi fechado.%n" + e.getStackTrace());
+//                }
+//            }
+//        }
+//    }
+
 }
